658181770685eb876497c618db213f19f47f62d4
clement fert <clementfert@hotmail.fr>
2022-02-13 22:40:36 +0000

    layout okay debut du js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-class Player1{
+class Partie{
     constructor(user,score,dice,curent_score){
-        this.user='player1';
+        this.user= null;
         this.score=0;
         this.dice=0;
         this.curent_score=0;
@@ -10,9 +10,53 @@ class Player1{
 }
 
 
+$(()=>{
+    $('#new_game').click (function(){
+
+        let first_launch = new Partie('player1',0,0,0);
+        let player2 = new Partie('player2',0,0,0);
+
+         first_launch.curent_score = first_launch.curent_score + getRandomInt();
+        
+        alert('la partie commence votre premier lancer est ' + first_launch.curent_score );
+
+        $('#current_score1').html(first_launch.curent_score);
+        $('#player1').css('color','red');
+
+
+        $('#rolle_dice').click(function(){
+            if (first_launch.curent_score >0 ){
+    
+                first_launch.curent_score = first_launch.curent_score + getRandomInt();
+                $('#current_score1').html(first_launch.curent_score);
+    
+            }
+            else {
+                first_launch.curent_score=0;
+                $('#current_score1').html(first_launch.curent_score);
+
+            }
+    
+        });
+
+      
 
 
 
+
+
+  
+
+    });
+
+
+
+
+
+
+
+
+});
 
 
 
@@ -20,9 +64,28 @@ class Player1{
 // et une valeur max (exclue).
 // Attention : si on utilisait Math.round(), on aurait une distribution
 // non uniforme !
-function getRandomInt(min, max) {
-    min = Math.ceil(1);
-    max = Math.floor(6);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-  console.log(getRandomInt)
+        function getRandomInt(min, max) {
+            min = Math.ceil(1);
+            max = Math.floor(7);
+            let result = Math.floor(Math.random() * (max - min)) + min;
+            if (result > 1){
+                return result;
+                }
+            else
+                {
+            return 0; 
+                }
+                
+            }
+      
+
+
+
+        
+        console.log(getRandomInt())
+       
+
+
+
+
+
